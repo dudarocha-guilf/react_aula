c047a209b2272a0eb9a7fd2293d34e71ec9d015f
Maria Eduarda <maria.e.rocha23@aluno.senai.br>
2026-04-24 10:28:09 -0300

    recreio

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import Header from './componentes/Header';
 import styled from 'styled-components';
 
-const AppContainer = `
+//constante que guarda os estilos(styled components)
+const AppContainer = styled.div`
   height: 100vw;
   width: 100vw;
   background-image: linear-gradient(90deg, #002f52 35%, #326589);
